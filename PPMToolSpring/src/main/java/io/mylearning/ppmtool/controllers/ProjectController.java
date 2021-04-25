1bd49ea8556aadcd510adb96f559f7f2a7234d7f
ML\jc94 <abhilash.kumar@sunlife.com>
2021-04-25 14:35:42 +0530

    Added Binding Result and used Pojo Validation using validation api

--- PPMToolSpring/src/main/java/io/mylearning/ppmtool/controllers/ProjectController.java
+++ PPMToolSpring/src/main/java/io/mylearning/ppmtool/controllers/ProjectController.java
@@ -1,8 +1,11 @@
 package io.mylearning.ppmtool.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +23,11 @@ public class ProjectController {
 	private ProjectService projectSerice;
 	
 	@PostMapping("/saveProject")
-	public ResponseEntity<Project> createProject(@RequestBody Project p){
+	public ResponseEntity<?> createProject(@Valid @RequestBody Project p,BindingResult result){
+		
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>( "Invalid Object",HttpStatus.BAD_REQUEST);
+		}
 		
 		Project project = projectSerice.createOrUpdateProject(p);
 		return new ResponseEntity<Project>(project,HttpStatus.CREATED);
